01deb4f07caa0813e9cc0ef0566669e8f8cd8e71
Hari143mani <121375212+Hari-mani-don@users.noreply.github.com>
2023-09-07 08:11:08 +0530

    Create StringProgram.java

---
+++ StringProgram.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String args[]){
+        String username="HariMani";
+        String password="HariDone!";
+        
+        System.out.println("your name is "+ username + "your password is "+ password );
+    }
+}
