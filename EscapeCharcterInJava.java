19fb55905757a12bdc3125911a570df5ed176608
Hari143mani <121375212+Hari-mani-don@users.noreply.github.com>
2023-09-07 08:31:18 +0530

    Create EscapeCharcterInJava.java

---
+++ EscapeCharcterInJava.java
@@ -0,0 +1,12 @@
+//Escape Character in java
+
+public class Main{
+    public static void main(String args[]){
+        String username="HariMani";
+        String password="HariDone!";
+        
+        System.out.println("your name is \"" + username + "\" your password is \"" + password + "\"");
+        System.out.println("hari\tmani");
+        System.out.println("hari ~\\mani\\done\\hairmani");
+    }
+}
