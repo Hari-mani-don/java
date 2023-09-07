842900ee8682363d85a3261eb1afda15a83f23f2
Hari143mani <121375212+Hari-mani-don@users.noreply.github.com>
2023-09-07 08:42:35 +0530

    Create InputGetFromUser.java

---
+++ InputGetFromUser.java
@@ -0,0 +1,12 @@
+//Input get from user  in java
+
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in) ;
+        System.out.print("Entery you name: ");
+        String hari = sc.nextLine();
+        
+        System.out.println("\n" + hari);
+    }
+}
