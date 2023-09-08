4f9afcfb47ed72fe925bdfbb28cf7daa41205c7e
Hari143mani <121375212+Hari-mani-don@users.noreply.github.com>
2023-09-08 07:55:08 +0530

    Create StringArrayScan.java

---
+++ StringArrayScan.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner scan = new Scanner(System.in);
+	    System.out.print("Enter a Integer: ");
+	    
+	    String a= new String();
+	    a = scan.nextLine();
+	    
+	    
+	    System.out.println("You are entered :" + a);
+	}
+}
