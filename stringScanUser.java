37927bbd8dd3322f30384090fb052f0e327cbf26
Hari143mani <121375212+Hari-mani-don@users.noreply.github.com>
2023-09-08 07:47:11 +0530

    Create stringScanUser.java

---
+++ stringScanUser.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner scan = new Scanner(System.in);
+	    System.out.print("Enter a String : ");
+	    
+         String a = scan.nextLine();
+	    
+	    
+	    System.out.println("You are entered :" + a);
+	}
+}
