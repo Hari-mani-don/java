705af1be4ad8fc329b1d0b2795c978b46f8d4331
Hari143mani <121375212+Hari-mani-don@users.noreply.github.com>
2023-09-08 18:16:51 +0530

    Create methodO0verloading.java

---
+++ methodO0verloading.java
@@ -0,0 +1,35 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+	  Main hari = new Main();
+	  hari.welcome(10, 20);
+	  hari.welcome(20.0f, 20);
+	    
+	}
+    void welcome(int a, int b){
+        System.out.println(" -------------------------------------------------");
+        System.out.println("|                                                 |");
+        System.out.println("|                                                 |");
+        System.out.println("|                                                 |");
+        System.out.println("|         ___________WELCOME_________             |");
+        System.out.println("|                                                 |");
+        System.out.println("|                                                 |");
+        System.out.println("|                                                 |");
+        System.out.println("|                                                 |");
+        System.out.println(" -------------------------------------------------");
+        System.out.println("                                                     \n");
+     }
+      void welcome(float a, int b){
+        System.out.println(" -------------------------------------------------");
+        System.out.println("|                                                 |");
+        System.out.println("|                                                 |");
+        System.out.println("|                                                 |");
+        System.out.println("|         ___________HARIMANI:)_________          |");
+        System.out.println("|                                                 |");
+        System.out.println("|                                                 |");
+        System.out.println("|                                                 |");
+        System.out.println("|                                                 |");
+        System.out.println(" -------------------------------------------------");
+     }
+}
