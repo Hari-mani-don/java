023c409fb70a244b449bc5ad3c98f4c441cde274
Hari143mani <121375212+Hari-mani-don@users.noreply.github.com>
2023-09-08 07:48:05 +0530

    Create longNormalScan.java

---
+++ longNormalScan.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner scan = new Scanner(System.in);
+	    System.out.print("Enter a Long Value: ");
+	    
+	    long a = scan.nextLong();
+	    
+	    
+	    System.out.println("You are entered :" + a);
+	}
+}
