c775cc08d4fdd68f2d71d2467c8debe5403f6fd1
Hari143mani <121375212+Hari-mani-don@users.noreply.github.com>
2023-09-08 08:48:53 +0530

    Create floatFormate.2f.java

---
+++ floatFormate.2f.java
@@ -0,0 +1,14 @@
+//float print in formate .2f
+
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner scan = new Scanner(System.in);
+	    
+	    float number = 123.4534566f;
+	    String formateNumber = String.format("%.2f", number);
+	    System.out.printf(formateNumber);
+	    
+	}
+}
