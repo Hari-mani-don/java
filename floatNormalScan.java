7e4148c83296053d51201d0062c7f9733c5e3233
Hari143mani <121375212+Hari-mani-don@users.noreply.github.com>
2023-09-08 07:52:02 +0530

    Create floatNormalScan.java

---
+++ floatNormalScan.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner scan = new Scanner(System.in);
+	    System.out.print("Enter a float: ");
+	    
+	    float a = scan.nextFloat();
+	    
+	    
+	    
+	    System.out.println("You are entered :" + a);
+	}
+}
