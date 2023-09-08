fe421d372b6fbc4ad53df4c1c327d4e0b02c7082
Hari143mani <121375212+Hari-mani-don@users.noreply.github.com>
2023-09-08 07:53:21 +0530

    Create integerNormalScan.java

---
+++ integerNormalScan.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner scan = new Scanner(System.in);
+	    System.out.print("Enter a Integer: ");
+	    
+	    int a = scan.nextInt();
+	    
+	    
+	    System.out.println("You are entered :" + a);
+	}
+}
