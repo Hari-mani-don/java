c2f3925aa554919e7b2913843b5c09cb690e5ec4
Hari143mani <121375212+Hari-mani-don@users.noreply.github.com>
2023-09-11 07:22:19 +0530

    Create boxClass.java

---
+++ boxClass.java
@@ -0,0 +1,47 @@
+public class Box{
+    private int length =10;
+    private String name;
+       
+    Box(int a, String name){
+       length = a;
+       this.name = name;
+    }
+    // public Box doubleBox(){
+    //     Box temp = new Box(2*this.length);
+    //     return temp;
+    // }
+    // public int minValue(int... vals){
+    //      int min=vals[0];
+    //      for(int k :vals){
+    //          if(k < min)
+    //             min = k;
+    //      }
+    //      return min;
+    // }
+    //   public double minValue(double... vals){
+    //     double max=vals[0];
+    //      for(double k : vals){
+    //          if(k > max)
+    //             max = k;
+    //      }
+    //      return max;
+    //  }
+//    public boolean isEqual(Box b){
+//        if(this.length == b.length)
+//            return true;
+//        return false;   
+//    }
+    //  public static void display(){
+    //     System.out.println("hariMani");
+    //  }
+    // public boolean isEquals(Box a){
+    //      if(this.length==a.length && this.name== a.name)
+    //         return true;
+       
+    //      return false;
+    //  }
+   
+
+    // }
+    
+}
