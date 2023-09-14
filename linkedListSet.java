0ceb1aee3a59218b64cf724f0aec1bedd3e62146
Hari143mani <121375212+Hari-mani-don@users.noreply.github.com>
2023-09-14 13:30:58 +0530

    Create linkedListSet.java

---
+++ linkedListSet.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main{
+	public static void main(String args[]) {
+		LinkedList<String> li = new LinkedList<>();
+		li.add("123");
+		li.add("456");
+		li.add("789");
+		li.set(0,"000");
+	    System.out.println(li);
+		
+	}
+}
