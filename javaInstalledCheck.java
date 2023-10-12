fa07403c52416c45100c2511ab7539e3b9f13a7b
Hari143mani <121375212+Hari-mani-don@users.noreply.github.com>
2023-10-12 06:46:01 +0530

    Create javaInstalledCheck.java

---
+++ javaInstalledCheck.java
@@ -0,0 +1,15 @@
+package Mani;
+
+
+
+public class New{    
+    public static void main(String[] args) {
+    	
+    	System.out.println("System Version : "+ System.getProperty("java.version"));
+    	System.out.println("Java Runtime Version : "+System.getProperty("java.runtime.version"));
+    	System.out.println("Java home : "+System.getProperty("java.home"));
+    	System.out.println("Java vendore : "+System.getProperty("java.vendor"));
+    	System.out.println("Java vendore url : "+System.getProperty("java.vendor.url"));
+    	System.out.println("Java class path : "+System.getProperty("java.class.path"));
+    }
+}
